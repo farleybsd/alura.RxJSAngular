4fd679c212c4e30b98a87ec46990c5ae8787448b
farleybsd <farley.t.i@hotmail.com>
2023-06-02 15:22:26 -0300

    observer

--- rxjs-projeto-base/2685-angular-rxjs-projeto-base/src/app/views/lista-livros/lista-livros.component.ts
+++ rxjs-projeto-base/2685-angular-rxjs-projeto-base/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { LivroService } from 'src/app/service/livro.service';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
+  campousca: string = '';
+  subscription: Subscription;
 
-  constructor() { }
+  constructor(private service: LivroService) { }
 
+  buscarLivros() 
+  {
+    this.subscription = this.service.buscar(this.campousca).subscribe({
+      next: retornoApi => {
+        console.log(retornoApi);
+      },
+      error: error => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('Observable Completado');
+      }
+    });
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
 
 
